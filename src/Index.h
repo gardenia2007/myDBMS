ad83a82ebfadfd2a38342649fd33319bcec228ad
李洪祥 <gardeniaxy@gmail.com>
2012-10-17 10:47:27 +0800

    添加Index类，负责索引操作

---
+++ src/Index.h
@@ -0,0 +1,23 @@
+/*
+ * Index.h
+ *
+ * 索引
+ *
+ *  Created on: 2012-10-17
+ *      Author: y
+ */
+
+#ifndef INDEX_H_
+#define INDEX_H_
+
+class Index {
+public:
+	Index();
+	virtual ~Index();
+
+	int getBlock(Data *); // 根据条件返回block指针（即块号）
+
+
+};
+
+#endif /* INDEX_H_ */
