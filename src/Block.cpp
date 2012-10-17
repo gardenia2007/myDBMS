510b2f38573a0e1648124d008f406ead2d062931
李洪祥 <gardeniaxy@gmail.com>
2012-10-17 12:28:35 +0800

    add Block class

---
+++ src/Block.cpp
@@ -0,0 +1,18 @@
+/*
+ * Block.cpp
+ *
+ *  Created on: 2012-10-17
+ *      Author: y
+ */
+
+#include "Block.h"
+
+Block::Block() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Block::~Block() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Block.h
@@ -0,0 +1,23 @@
+/*
+ * Block.h
+ *
+ * 封装文件块操作
+ *
+ *  Created on: 2012-10-17
+ *      Author: y
+ */
+
+#ifndef BLOCK_H_
+#define BLOCK_H_
+
+class Block {
+public:
+	Block();
+	virtual ~Block();
+
+private:
+	void readBlock(int);
+	void writeBlock(int);
+};
+
+#endif /* BLOCK_H_ */
