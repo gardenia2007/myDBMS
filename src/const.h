c03657d18815fcb864ed74cc9b4baa7fcf5b16f5
李洪祥 <gardeniaxy@gmail.com>
2012-10-19 15:01:43 +0800

    db path problem in windows

--- src/const.h
+++ src/const.h
@@ -20,7 +20,7 @@
 #define DATA_PATH		"/home/y/workspace/myDBMS/db/"
 #define PATH_SPARATOR	"/"
 #elif WIN32 // windows下
-#define DATA_PATH		"D:\\myDBMS\\db"
+#define DATA_PATH		"D:\\myDBMS\\db\\"
 #define PATH_SPARATOR	"\\"
 #endif
 
